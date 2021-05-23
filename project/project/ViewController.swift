ec7e301bdc548790636be450423c6df545099e5a
Ho Viet Long <longkunz@Hos-Mac.local>
2021-05-23 16:06:12 +0700

    file dau then

---
+++ project/project/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  project
+//
+//  Created by Ho Viet Long on 5/23/21.
+//  Copyright © 2021 tranthihoaitrang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // hello
+    }
+
+
+}
+
